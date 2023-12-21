a40cd9bba53ec46a006352b5be8f9846d8d6247a
Denitsa Marinova <Denitsa.Marinova@multichoice.co.za>
2023-12-21 18:22:59 +0200

    Client login and register

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+  users: any;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInIt(): void {
+    this.getUsers()
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+ @Input() usersFromHomeComponent: any;
+ @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor() {}
+
+  ngOnInIt(): void {
+
+  }
+
+  register() {
+    console.log(this.model);
+    
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);    
+  }
+
+}
