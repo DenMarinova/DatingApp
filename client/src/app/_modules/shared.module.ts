74bb1d2626b3c5a5108b9feba6c6e8a1a3cb1bd0
Denitsa Marinova <Denitsa.Marinova@multichoice.co.za>
2024-01-11 15:49:09 +0200

    User Interface for members

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -8,12 +9,14 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot(
       {positionClass: "toast-bottom-right"}
     )
   ],
   exports: [
     BsDropdownModule,
+    TabsModule,
     ToastrModule
   ]
 })
--- client/src/app/member/member-detail/member-detail.component.ts
+++ client/src/app/member/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent {
+  member: Member | undefined;
 
+  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember() {
+    var username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    })
+  }
 }
