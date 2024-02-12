8484f27e426ddfbdf9469ae4182e1d720e33386d
Denitsa Marinova <Denitsa.Marinova@multichoice.co.za>
2024-02-12 20:07:54 +0200

    Edit Profile, Spinner

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using AutoMapper;
 using API.DTOs;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -33,6 +34,21 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDTO>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user==null) return NotFound();
+
+            _mapper.Map(memberUpdateDTO, user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user.");
         }
     }
 }
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
@@ -12,12 +13,14 @@ import { ToastrModule } from 'ngx-toastr';
     TabsModule.forRoot(),
     ToastrModule.forRoot(
       {positionClass: "toast-bottom-right"}
-    )
+    ),
+    NgxSpinnerModule.forRoot({ type: 'ball-beat' })
   ],
   exports: [
     BsDropdownModule,
     TabsModule,
-    ToastrModule
+    ToastrModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
